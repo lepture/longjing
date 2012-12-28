d9fca3cceffc41a52c05f6620ed0594f152a1ddc
Hsiaoming Yang <lepture@me.com>
2012-12-29 04:53:30 +0800

    prepare for tests

---
+++ tests/testutils.js
@@ -0,0 +1,7 @@
+require('colorful').logging.config('error');
+module.exports = function(file) {
+  if (process.env.COVERAGE) {
+    file = file.replace('/lib/', '/lib-cov/');
+  }
+  return require(file);
+};
